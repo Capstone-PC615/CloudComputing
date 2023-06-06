8b22ee52a58dbd8c8430d4c012d72ac29ff5478d
Raihan <c297dsx4862@bangkit.academy>
2023-06-06 22:32:53 +0700

    initial commit

--- sqlserver.js
+++ sqlserver.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
 const dbhost = cre.env.DB_HOST
 const dbuser = cre.env.DB_USER
 const dbpass = cre.env.DB_PASSWORD
@@ -22,3 +23,66 @@ db.connect((err) => {
     console.log('Connected to database')
 }
 )
+
+app.use(express.json())
+
+//Register for new user
+app.post('/register', (req, res) => {
+    const username = req.body.username;
+    const hashedPasswrord = await bcrypt.hash(req.body.password, 10);
+
+    db.getConnection((err, connection) => {
+        if (err) throw (err)
+
+        const dbSearch = 'SELECT * FROM users WHERE username = ?'
+        const search_query = mysql.format(dbSearch, [username])
+
+        const dbInsert = 'INSERT INTO users (username, password) VALUES (?, ?)'
+        const insert_query = mysql.format(dbInsert, [username, hashedPasswrord])
+
+        await connection.query(search_query, (err, result) => {
+            if (err) throw (err)
+            if (result.length > 0) {
+                connection.release()
+                console.log('User already exists')
+                res.send(409)
+            } else {
+                await connection.query(insert_query, (err, result) => {
+                    connection.release()
+                    if (err) throw (err)
+                    console.log('User created')
+                    console.log(result.insertId)
+                    res.send(201)
+                })
+            }
+        })
+    })
+})
+
+//Login
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.getConnection((err, connection) => {
+        if (err) throw (err)
+
+        const dbSearch = 'SELECT * FROM users WHERE username = ?'
+        const search_query = mysql.format(dbSearch, [username])
+
+        connection.query(search_query, (err, result) => {
+            if (err) throw (err)
+            if (result.length > 0) {
+                bcrypt.compare(password, result[0].password, (err, response) => {
+                    if (response) {
+                        res.send('Logged in')
+                    } else {
+                        res.send('Incorrect username or password')
+                    }
+                })
+            } else {
+                res.send('Incorrect username or password')
+            }
+        })
+    })
+})
